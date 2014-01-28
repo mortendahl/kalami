88cfa43eac413d0cd7d0c69d03d5a937fea7478e
Morten Dahl <douglas.botley@gmail.com>
2014-01-28 16:02:56 +0100

    clean up

--- evaluation.ml
+++ evaluation.ml
@@ -5,7 +5,7 @@ open Printing
 
     
 
-(* ********** Evaluation ********** *)
+(* ********** Diverse helper functions ********** *)
 
 
 (* repeated squaring *)
@@ -26,7 +26,89 @@ let power base exponent =
                     helper base' exponent' acc'
     in
     helper base exponent 1
+
+
+
+
+(* projecting a number into a tuple with specified arity *)
+let project arity number =
     
+    (* returns the base-10 digits of number as a list, ie 'getdigits 12345' gives [5;4;3;2;1] *)
+    let rec getdigits number =
+        if (=) 0 number then [] 
+        else
+            let rightmost_digit = number mod 10 in
+            rightmost_digit :: (getdigits (number / 10))
+    in
+    
+    (* inverse of 'getdigits', ie 'getnumber [5;4;3;2;1]' gives 12345 *)
+    let rec getnumber digits =
+        match digits with
+            [] -> 0
+            | rightmost_digit :: rest ->
+                (getnumber rest) * 10 + rightmost_digit
+    in
+    
+    (* selects every 'interval' number, starting at 'offset' from 'digits',
+         ie  'skip_select 2 [5;4;3;2;1] 1' gives [4;2]
+         and 'skip_select 2 [5;4;3;2;1] 0' gives [5;3;1]   *)
+    let skip_select interval digits offset =
+        let rec helper digits offset =
+            match digits with
+                [] -> []
+                | ele::rest ->
+                    if (=) 0 offset then 
+                        ele :: (helper rest (interval - 1))
+                    else
+                        helper rest (offset - 1)
+        in
+        helper digits offset
+    in
+    
+    (* projection of 'number' into 'arity' components, ie 'project 2 12345' gives [24; 135] *)
+    let digits = getdigits number in
+    let rec helper component =
+        if component < 1 then []
+        else
+            (getnumber (skip_select arity digits (component - 1))) :: (helper (component - 1))
+    in
+    helper arity
+
+
+
+
+(* finds the identifiers for guesses without an upper bound in a statement *)
+let rec get_unbounded_guesses stmt =
+    match stmt with
+        StmtLet(_, _, stmt) ->
+            get_unbounded_guesses stmt
+            
+        | StmtIf(_, stmt_true, stmt_false) ->
+            (get_unbounded_guesses stmt_true) @ (get_unbounded_guesses stmt_false)
+            
+        | StmtGuess(id, stmt) ->
+            id :: (get_unbounded_guesses stmt)
+            
+        | StmtGuessLowerBound(id, _, stmt) ->
+            id :: (get_unbounded_guesses stmt)
+            
+        | StmtGuessLowerUpperBound(_, _, _, stmt) ->
+            get_unbounded_guesses stmt
+            
+        | StmtAccept ->
+            []
+            
+        | StmtReject ->
+            []
+    
+    
+
+
+
+
+
+
+(* ********** Evaluation ********** *)
 
 
 let rec eval_expr env expr =
@@ -143,74 +225,6 @@ let rec eval_stmt env guesses stmt =
             
         | StmtReject ->
             None
-
-
-let project arity number =
-    
-    (* returns the base-10 digits of number as a list, ie 'getdigits 12345' gives [5;4;3;2;1] *)
-    let rec getdigits number =
-        if (=) 0 number then [] 
-        else
-            let rightmost_digit = number mod 10 in
-            rightmost_digit :: (getdigits (number / 10))
-    in
-    
-    (* inverse of 'getdigits', ie 'getnumber [5;4;3;2;1]' gives 12345 *)
-    let rec getnumber digits =
-        match digits with
-            [] -> 0
-            | rightmost_digit :: rest ->
-                (getnumber rest) * 10 + rightmost_digit
-    in
-    
-    (* selects every 'interval' number, starting at 'offset' from 'digits',
-         ie  'skip_select 2 [5;4;3;2;1] 1' gives [4;2]
-         and 'skip_select 2 [5;4;3;2;1] 0' gives [5;3;1]   *)
-    let skip_select interval digits offset =
-        let rec helper digits offset =
-            match digits with
-                [] -> []
-                | ele::rest ->
-                    if (=) 0 offset then 
-                        ele :: (helper rest (interval - 1))
-                    else
-                        helper rest (offset - 1)
-        in
-        helper digits offset
-    in
-    
-    (* projection of 'number' into 'arity' components, ie 'project 2 12345' gives [24; 135] *)
-    let digits = getdigits number in
-    let rec helper component =
-        if component < 1 then []
-        else
-            (getnumber (skip_select arity digits (component - 1))) :: (helper (component - 1))
-    in
-    helper arity
-
-
-let rec get_unbounded_guesses stmt =
-    match stmt with
-        StmtLet(_, _, stmt) ->
-            get_unbounded_guesses stmt
-            
-        | StmtIf(_, stmt_true, stmt_false) ->
-            (get_unbounded_guesses stmt_true) @ (get_unbounded_guesses stmt_false)
-            
-        | StmtGuess(id, stmt) ->
-            id :: (get_unbounded_guesses stmt)
-            
-        | StmtGuessLowerBound(id, _, stmt) ->
-            id :: (get_unbounded_guesses stmt)
-            
-        | StmtGuessLowerUpperBound(_, _, _, stmt) ->
-            get_unbounded_guesses stmt
-            
-        | StmtAccept ->
-            []
-            
-        | StmtReject ->
-            []
 
 
 let eval stmt =
